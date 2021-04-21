99b6dd28f317249ed740447f1c4dc96a52e5efbe
huey.j <huey.j@kakaocorp.com>
2021-04-21 23:05:50 +0900

    ALGO-38 config 수정

--- actions.py
+++ actions.py
@@ -36,15 +36,17 @@ class ActionAlgorithmExplain(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        algorithm_type = tracker.latest_message['entities']['algorithm_type']['value']
+        algorithm_type = tracker.latest_message['entities'][0]['value']
 
+        print(tracker.latest_message)
+        print(tracker.latest_message['entities'])
         explain_text = "잘 모르곘어..."
         if algorithm_type == "정렬":
             explain_text="정렬은 원소값을 오름차순이나 내림차순으로 수를 나열하는 알고리즘이야."
         elif algorithm_type == "최단거리":
             explain_text="최단거리 알고리즘은 네트워크에서 하나의 시작 정점으로부터 모든 다른 정점까지의 최단 경로를 찾는 알고리즘이야."
         elif algorithm_type == "스택":
-            explain_text="스택은 스택은 마지막에 들어온 것이 먼저 나가는 LIFO(Last In First Out) 구조를 가진 자료 구조야"
+            explain_text="스택은 마지막에 들어온 것이 먼저 나가는 LIFO(Last In First Out) 구조를 가진 자료 구조야"
 
         dispatcher.utter_message(explain_text)
 
