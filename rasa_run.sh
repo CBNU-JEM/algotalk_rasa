360d5a8b20d7a3eca58c7458d107dce3ff54402e
junho <jh280722@naver.com>
2021-08-18 23:02:01 +0900

    rasa run script 수정

--- rasa_run.sh
+++ rasa_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf "rasa run endpoints\n"
-rasa run --endpoints endpoints.yml > rasa.log 2>&1 &
+rasa run --endpoints endpoints.yml > rasa.log 2>rasa_error.log &
 
 printf "rasa run actions\n"
-rasa run actions --actions actions.actions > rasa_actions.log 2>&1
+rasa run actions --actions actions.actions > rasa_actions.log 2>rasa_actions_error.log
