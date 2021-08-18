dd9924b1e04e4f8faa50f6f94d0257aefdfe87bc
junho <jh280722@naver.com>
2021-08-18 22:41:37 +0900

    rasa run script 수정

--- rasa_run.sh
+++ rasa_run.sh
@@ -4,4 +4,4 @@ printf "rasa run actions\n"
 nohup rasa run actions --actions actions.actions > rasa_actions.log 2>&1 &
 
 printf "rasa run endpoints\n"
-nohup run --endpoints endpoints.yml > rasa.log 2>&1 &
+nohup run --endpoints endpoints.yml > rasa.log 2>&1
