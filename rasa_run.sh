b272a1ec1e5c370641da80b52d20651a3de91958
junho <jh280722@naver.com>
2021-08-21 15:00:26 +0900

    rasa_run script에 cp, git pull 추가

--- rasa_run.sh
+++ rasa_run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-printf "rasa run endpoints\n"
-rasa run --endpoints endpoints.yml > rasa.log 2>rasa_error.log &
+git reset --hard
+printf "git reset hard\n"
 
-printf "rasa run actions\n"
+git pull origin main
+printf "git pull algotalk_rasa\n"
+
+cp korean_tokenizer.py $RASA_DIR/nlu/tokenizers
+cp crf_entity_extractor_korean.py $RASA_DIR/nlu/extractors
+cp bilou_utils_Kr.py $RASA_DIR/nlu/utils
+cp registry.py $RASA_DIR/nlu
+printf "copy rasa_config\n"
+
+rasa run --endpoints endpoints.yml > rasa.log 2>rasa_error.log &
+printf "rasa run endpoints\n"
+
 rasa run actions --actions actions.actions > rasa_actions.log 2>rasa_actions_error.log
+printf "rasa run actions\n"
