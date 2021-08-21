3c0b38144025a5decd4837352051c27a193679ef
junho <jh280722@naver.com>
2021-08-21 15:13:56 +0900

    rasa_run script에 cp, git pull 추가

--- rasa_run.sh
+++ rasa_run.sh
@@ -3,10 +3,10 @@ cp korean_tokenizer.py $RASA_DIR/nlu/tokenizers
 cp crf_entity_extractor_korean.py $RASA_DIR/nlu/extractors
 cp bilou_utils_Kr.py $RASA_DIR/nlu/utils
 cp registry.py $RASA_DIR/nlu
-printf "copy rasa_config\n"
+printf "copy rasa_config complete\n"
 
-rasa run --endpoints endpoints.yml > rasa.log 2>rasa_error.log &
 printf "rasa run endpoints\n"
+rasa run --endpoints endpoints.yml > rasa.log 2>rasa_error.log &
 
-rasa run actions --actions actions.actions > rasa_actions.log 2>rasa_actions_error.log
 printf "rasa run actions\n"
+rasa run actions --actions actions.actions > rasa_actions.log 2>rasa_actions_error.log
