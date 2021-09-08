1c328a88e979a976f5dfb03954dd4c036cb4fcb5
huey.j <huey.j@kakaocorp.com>
2021-09-08 22:35:29 +0900

    ALGO-59 db 테이블 수정 (problem content를 uri로 변경)

--- actions/insert_problem_algorithm.py
+++ actions/insert_problem_algorithm.py
@@ -1,9 +1,11 @@
 import json
 import db
 
-with open('problem_algorithm.json') as json_file:
+with open('../baekjoon_crawling/json/url_of_problem_per_algorithm.json') as json_file:
     json_data = json.load(json_file)
     algorithm_problem_list = set()
 
     for problem in json_data['problem']:
-        db.create_algorithm_problem_classification([db.AlgorithmProblemClassification(None, problem['classification'], problem['problem_title'])])
+        algorithm_name = problem['classification']
+        algorithm_id = db.execute_query(f'SELECT ID FROM ALGORITHM WHERE NAME = "{algorithm_name}"')
+        db.create_algorithm_problem_classification([db.AlgorithmProblemClassification(None, algorithm_id[0][0], problem['problem_id'])])
