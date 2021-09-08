fa65fd669b7c160d0264d3e1a7376a272ff88842
JunHo, Jeon <49743053+jh280722@users.noreply.github.com>
2021-09-08 23:45:21 +0900

    Update insert_problem_algorithm.py

--- actions/insert_problem_algorithm.py
+++ actions/insert_problem_algorithm.py
@@ -10,7 +10,7 @@ with open('../baekjoon_crawling/json/url_of_problem_per_algorithm.json') as json
         rows = db.execute_query(f'SELECT ID FROM ALGORITHM WHERE NAME = "{algorithm_name}"')
 
         problem_id = problem['problem_id']
-        rows2 = db.execute_query(f'SELECT ID FROM ALGORITHM WHERE NAME = "{problem_id}"')
+        rows2 = db.execute_query(f'SELECT ID FROM PROBLEM WHERE NAME = "{problem_id}"')
 
         if rows and rows2:
             db.create_algorithm_problem_classification([db.AlgorithmProblemClassification(None, rows[0][0], rows2[0][0])])
