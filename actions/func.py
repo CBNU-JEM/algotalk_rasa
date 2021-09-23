4b60278c6d1827cf29a4a5fe97af6977c50b8982
huey.j <huey.j@kakaocorp.com>
2021-09-23 22:49:20 +0900

    ALGO-59 db 변경으로 인한 actions 로직 수정

--- actions/func.py
+++ actions/func.py
@@ -17,18 +17,22 @@ def level_up(level="실버"):
 
 
 changed_level = dict()
-key = ["브론즈", "실버", "골드", "플레티넘", "플레", "다이아", "상", "중", "하", "초급", "중급", "고급", "쉬움", "쉬운", "쉬운거", "쉬운 것", "어려움",
-       "어려운거", "어려운 것", "어려운"]
-value = ["브론즈", "실버", "골드", "플레티넘", "플레티넘", "다이아", "플레티넘", "골드", "실버", "실버", "골드", "플레티넘", "실버", "실버", "실버", "실버",
-         "플레티넘", "플레티넘", "플레티넘", "플레티넘"]
+# key = ["브론즈", "실버", "골드", "플레티넘", "플레", "다이아", "상", "중", "하", "초급", "중급", "고급", "쉬움", "쉬운", "쉬운거", "쉬운 것", "어려움",
+#       "어려운거", "어려운 것", "어려운"]
+# value = ["브론즈", "실버", "골드", "플레티넘", "플레티넘", "다이아", "플레티넘", "골드", "실버", "실버", "골드", "플레티넘", "실버", "실버", "실버", "실버",
+#         "플레티넘", "플레티넘", "플레티넘", "플레티넘"]
+
+key = ["브론즈", "실버", "골드", "플레티넘", "다이아"]
+value = [1, 6, 11, 16, 21]
 
 for i in range(len(key)):
     changed_level[key[i]] = value[i]
 
 
 def level_mapping(level):
-    print(changed_level.get(level, None))
-    return changed_level.get(level, None)
+    print(changed_level.get(level, 0))
+    return changed_level.get(level, 0)
+
 
 class OverlapProblem:
     def __init__(self, problem=None):
